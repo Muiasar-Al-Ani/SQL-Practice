d5d835df3353ba562adc92ad65c6dde34af3a22b
Muiasar-Al-Ani <82427073+Muiasar-Al-Ani@users.noreply.github.com>
2021-08-19 15:35:34 -0400

    Added the customers_db and it's tables the customers and the customers_orders and made the connections as of the primary and the foreign keys

--- sql-practice/01-Activities/20-Stu_Foreign-Primary-Key/Unsolved/db/schema.sql
+++ sql-practice/01-Activities/20-Stu_Foreign-Primary-Key/Unsolved/db/schema.sql
@@ -1,1 +1,23 @@
 -- Write your Schema Here -- 
+DROP DATABASE IF EXISTS customers_db;
+
+CREATE DATABASE customers_db;
+
+USE customers_db;
+
+CREATE TABLE customers (
+    id INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30)
+
+);
+
+CREATE TABLE customer_orders (
+    id INT NOT NULL PRIMARY KEY,
+    customer_id INT NOT NULL,
+    order_details TEXT NOT NULL,
+    FOREIGN KEY (customer_id) 
+    REFERENCES customers(id) 
+    ON DELETE SET NULL
+);
+
