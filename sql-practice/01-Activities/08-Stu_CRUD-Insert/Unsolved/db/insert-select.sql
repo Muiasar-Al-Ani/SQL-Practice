9694c9c3680f4d10ff0dbd6e50f4ad3788e6f348
Muiasar-Al-Ani <82427073+Muiasar-Al-Ani@users.noreply.github.com>
2021-08-19 11:02:42 -0400

    Added comments to explain what each line does

--- sql-practice/01-Activities/08-Stu_CRUD-Insert/Unsolved/db/insert-select.sql
+++ sql-practice/01-Activities/08-Stu_CRUD-Insert/Unsolved/db/insert-select.sql
@@ -1,7 +1,11 @@
+-- Inserts in the biographies table and choeses the id and the name columns
 INSERT INTO biographies (id, name)
+-- The chosed values are:
 VALUES
+-- Each row will be populated with id and name and the total will be 3 rows
     ( 001, "Diary of Anne Frank"),
     ( 002, "Frida: A Biography of Frida Kahlo"),
     ( 003, "Long Walk to Freedom");
 
+-- Returns all the biographies table
 SELECT * FROM biographies;
