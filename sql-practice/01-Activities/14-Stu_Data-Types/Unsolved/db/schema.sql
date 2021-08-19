0ed4a6fbd27a49cb44156e0303612e265b809cd1
Muiasar-Al-Ani <82427073+Muiasar-Al-Ani@users.noreply.github.com>
2021-08-19 12:14:36 -0400

    Added types and not null values to the customers table create function

--- sql-practice/01-Activities/14-Stu_Data-Types/Unsolved/db/schema.sql
+++ sql-practice/01-Activities/14-Stu_Data-Types/Unsolved/db/schema.sql
@@ -3,9 +3,9 @@ CREATE DATABASE customer_db;
 
 USE customer_db;
 
-CREATE TABLE customers (
-  id,
-  first_name,
-  last_name,
-  value_card_member
-);
+CREATE TABLE customers ( 
+  id INT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  value_card_member BOOLEAN NOT NULL 
+  );
