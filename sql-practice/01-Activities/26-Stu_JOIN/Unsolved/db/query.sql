b5a675a99bdff6b583191d7e5dffcaec1c3f0531
Muiasar-Al-Ani <82427073+Muiasar-Al-Ani@users.noreply.github.com>
2021-08-19 17:36:02 -0400

    Added a join method to the query.sql that joins two tables if the condition met

--- sql-practice/01-Activities/26-Stu_JOIN/Unsolved/db/query.sql
+++ sql-practice/01-Activities/26-Stu_JOIN/Unsolved/db/query.sql
@@ -1,1 +1,6 @@
 -- Add your code below and execute file in MySQL Shell --
+SELECT * FROM book_prices;
+SELECT * FROM favorite_books;
+SELECT *
+FROM favorite_books
+JOIN book_prices ON book_prices.price = favorite_books.book_price;
