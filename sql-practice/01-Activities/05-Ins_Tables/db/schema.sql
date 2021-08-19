1c68c670dc098a8ab9442a57392a261c486a8d09
Muiasar-Al-Ani <82427073+Muiasar-Al-Ani@users.noreply.github.com>
2021-08-19 10:49:34 -0400

    fixed bug in the activity 5

--- sql-practice/01-Activities/05-Ins_Tables/db/schema.sql
+++ sql-practice/01-Activities/05-Ins_Tables/db/schema.sql
@@ -8,8 +8,11 @@ USE inventory_db;
 -- Creates the table "produce" within inventory_db --
 CREATE TABLE produce (
   -- Creates a numeric column called "id" which will automatically increment its default value as we create new rows --
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   -- Makes a string column called "name" which cannot contain null --
   name VARCHAR(100) NOT NULL
 );
+
+
+
 
