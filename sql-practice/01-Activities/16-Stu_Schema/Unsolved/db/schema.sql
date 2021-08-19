656854b3bdbf9922c78af22a892b0f4048e267c3
Muiasar-Al-Ani <82427073+Muiasar-Al-Ani@users.noreply.github.com>
2021-08-19 12:37:54 -0400

    Added the inventory database with 2 tables the products and categories in the schema.sql

--- sql-practice/01-Activities/16-Stu_Schema/Unsolved/db/schema.sql
+++ sql-practice/01-Activities/16-Stu_Schema/Unsolved/db/schema.sql
@@ -1,1 +1,25 @@
 -- Write your schema here --
+
+DROP DATABASE IF EXISTS inventory_db;
+
+CREATE DATABASE inventory_db;
+
+USE inventory_db;
+
+DROP TABLE IF EXISTS products;
+
+CREATE TABLE products (
+    id INT NOT NULL,
+    product_name VARCHAR(30) NOT NULL,
+    category_name VARCHAR(100) NOT NULL,
+    price INT NOT NULL,
+    in_stock BOOLEAN NOT NULL
+);
+
+DROP TABLE IF EXISTS categories;
+
+CREATE TABLE categories (
+    id INT NOT NULL,
+    category_name VARCHAR(30) NOT NULL
+);
+
