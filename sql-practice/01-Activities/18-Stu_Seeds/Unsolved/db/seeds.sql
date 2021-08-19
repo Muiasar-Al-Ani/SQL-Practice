4c0b231bdec1bc06628f75132184f7ade8639daa
Muiasar-Al-Ani <82427073+Muiasar-Al-Ani@users.noreply.github.com>
2021-08-19 12:52:10 -0400

    Added the missing ids for the insert command to work proporly

--- sql-practice/01-Activities/18-Stu_Seeds/Unsolved/db/seeds.sql
+++ sql-practice/01-Activities/18-Stu_Seeds/Unsolved/db/seeds.sql
@@ -1,7 +1,7 @@
-INSERT INTO products (product_name, category_name)
-VALUES ("spinach", "produce"),
-       ("peanut butter", "staples"),
-       ("peas-canned", "canned goods"),
-       ("ice cream", "frozen"),
-       ("potato chips", "snacks");
+INSERT INTO products (id, product_name, category_name)
+VALUES (1000, "spinach", "produce"),
+       (1001, "peanut butter", "staples"),
+       (1002, "peas-canned", "canned goods"),
+       (1003, "ice cream", "frozen"),
+       (1004, "potato chips", "snacks");
        
